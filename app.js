0298dc81daccac849eb7c38fba353635ae7b2516
“camillebrown” <camillebrown11@gmail.com>
2020-09-23 20:09:25 -0700

    more css and prelim js code

---
+++ app.js
@@ -0,0 +1,50 @@
+//Start out with your questions
+// Make an array of all of the questions in each section of the game
+
+//keep track of the user's score
+let score = 0
+const question = document.querySelector()
+
+
+//==============ROUND 1: GUESS THE ARTIST===========//
+const round1Qs = [
+    'Which artist wrote the song "Californication"?',
+    'Which artist wrote the song "Stir Fry"?',
+    'Which artist wrote the song "California Love"?',
+    'Which artist wrote the song "Jolene"?',
+    'Which artist wrote the song "U Don\'t Have to Call"?',
+    'Which artist wrote the song "Country Girl(Shake It For Me)"?',
+    'Which artist wrote the song "No Scrubs"?',
+    'Which artist wrote the song "Cry Me A River"?',
+    'Which artist wrote the song "Thank U, Next"?',
+    'Which artist wrote the song "Stairway to Heaven"?',
+]
+
+//==============ROUND 2: FINISH THE LYRICS===========//
+const round1Qs = [
+    'Which artist wrote the song "Californication"?',
+    'Which artist wrote the song "Stir Fry"?',
+    'Which artist wrote the song "California Love"?',
+    'Which artist wrote the song "Jolene"?',
+    'Which artist wrote the song "U Don\'t Have to Call"?',
+    'Which artist wrote the song "Country Girl(Shake It For Me)"?',
+    'Which artist wrote the song "No Scrubs"?',
+    'Which artist wrote the song "Cry Me A River"?',
+    'Which artist wrote the song "Thank U, Next"?',
+    'Which artist wrote the song "Stairway to Heaven"?',
+]
+
+//==============ROUND 3: GENERAL MUSIC TRIVIA===========//
+const round1Qs = [
+    'Which artist wrote the song "Californication"?',
+    'Which artist wrote the song "Stir Fry"?',
+    'Which artist wrote the song "California Love"?',
+    'Which artist wrote the song "Jolene"?',
+    'Which artist wrote the song "U Don\'t Have to Call"?',
+    'Which artist wrote the song "Country Girl(Shake It For Me)"?',
+    'Which artist wrote the song "No Scrubs"?',
+    'Which artist wrote the song "Cry Me A River"?',
+    'Which artist wrote the song "Thank U, Next"?',
+    'Which artist wrote the song "Stairway to Heaven"?',
+]
+

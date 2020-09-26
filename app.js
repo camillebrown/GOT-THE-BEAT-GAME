68de726e3e26074c7882dcc1adda0b46e42abf33
“camillebrown” <camillebrown11@gmail.com>
2020-09-26 08:41:33 -0700

    added functionality to the game for correct and incorrect answers

--- app.js
+++ app.js
@@ -1,50 +1,162 @@
-//Start out with your questions
-// Make an array of all of the questions in each section of the game
-
-//keep track of the user's score
 let score = 0
-const question = document.querySelector()
+const question = document.querySelector('#question')
+const choices = Array.from(document.querySelectorAll('.choice-text'))
+const scoreText = document.querySelector('#scoring')
+const questionNum = document.querySelector('.question-number')
 
+let currentQuestion = {}
+let acceptingAnswers = true
+let questionCounter = 0
+let availableQuestions = []
+const score_points = 1
+const max_questions = 10
 
+//Start out with your questions
+// Make an array of objects with all of the questions, answer options and the correct answer
 //==============ROUND 1: GUESS THE ARTIST===========//
 const round1Qs = [
-    'Which artist wrote the song "Californication"?',
-    'Which artist wrote the song "Stir Fry"?',
-    'Which artist wrote the song "California Love"?',
-    'Which artist wrote the song "Jolene"?',
-    'Which artist wrote the song "U Don\'t Have to Call"?',
-    'Which artist wrote the song "Country Girl(Shake It For Me)"?',
-    'Which artist wrote the song "No Scrubs"?',
-    'Which artist wrote the song "Cry Me A River"?',
-    'Which artist wrote the song "Thank U, Next"?',
-    'Which artist wrote the song "Stairway to Heaven"?',
+    {
+        question:'Which artist wrote the song "Californication"?',
+        option1: 'Red Hot Chili Peppers',
+        option2: 'Foo Fighters',
+        option3: 'AC/DC',
+        option4: 'The Ramones',
+        correctAnswer: 1,
+    },
+    {
+        question:'Which artist wrote the song "Stir Fry"?',
+        option1: 'Trippie Red',
+        option2: 'Biggie Smalls',
+        option3: 'Migos',
+        option4: '2 Chainz',
+        correctAnswer: 3,
+    },
+    {
+        question:'Which artist wrote the song "California Love"?',
+        option1: 'Future',
+        option2: 'Run DMC',
+        option3: 'Jay-Z',
+        option4: 'Tupac',
+        correctAnswer: 4,
+    },
+    {
+        question:'Which artist wrote the song "Jolene"?',
+        option1: 'Carrie Underwood',
+        option2: 'Shania Twain',
+        option3: 'Dolly Parton',
+        option4: 'Kelly Clarkson',
+        correctAnswer: 3,
+    },
+    {
+        question:'Which artist wrote the song "U Don\'t Have to Call"?',
+        option1: 'Aaliyah',
+        option2: 'Usher',
+        option3: 'Beyoncé',
+        option4: 'Chris Brown',
+        correctAnswer: 2,
+    },
+    {
+        question:'Which artist wrote the song "Country Girl(Shake It For Me)"?',
+        option1: 'Keith Urban',
+        option2: 'Brad Paisley',
+        option3: 'Blake Shelton',
+        option4: 'Luke Bryan',
+        correctAnswer: 4,
+    },
+    {
+        question:'Which artist wrote the song "No Scrubs"?',
+        option1: 'TLC',
+        option2: 'Destiny\'s Child',
+        option3: 'SZA',
+        option4: 'Rihanna',
+        correctAnswer: 1,
+    },
+    {
+        question:'Which artist wrote the song "Cry Me A River"?',
+        option1: 'Robin Thicke',
+        option2: 'Justin Timberlake',
+        option3: 'Ed Sheeran',
+        option4: 'Sam Smith',
+        correctAnswer: 2,   
+    },
+    {
+        question:'Which artist wrote the song "Thank U, Next"?',
+        option1: 'Bebe Rexha',
+        option2: 'Dua Lipa',
+        option3: 'Billie Eilish',
+        option4: 'Ariana Grande',
+        correctAnswer: 4,   
+    },
+    {
+        question:'Which artist wrote the song "Stairway to Heaven"?',
+        option1: 'Pink Floyd',
+        option2: 'Rolling Stones',
+        option3: 'Led Zeppelin',
+        option4: 'Metallica',
+        correctAnswer: 3,   
+    }
 ]
 
-//==============ROUND 2: FINISH THE LYRICS===========//
-const round1Qs = [
-    'Which artist wrote the song "Californication"?',
-    'Which artist wrote the song "Stir Fry"?',
-    'Which artist wrote the song "California Love"?',
-    'Which artist wrote the song "Jolene"?',
-    'Which artist wrote the song "U Don\'t Have to Call"?',
-    'Which artist wrote the song "Country Girl(Shake It For Me)"?',
-    'Which artist wrote the song "No Scrubs"?',
-    'Which artist wrote the song "Cry Me A River"?',
-    'Which artist wrote the song "Thank U, Next"?',
-    'Which artist wrote the song "Stairway to Heaven"?',
-]
+//Create a function to start the game
+const startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...round1Qs]
+    getNewQuestion()
+}
 
-//==============ROUND 3: GENERAL MUSIC TRIVIA===========//
-const round1Qs = [
-    'Which artist wrote the song "Californication"?',
-    'Which artist wrote the song "Stir Fry"?',
-    'Which artist wrote the song "California Love"?',
-    'Which artist wrote the song "Jolene"?',
-    'Which artist wrote the song "U Don\'t Have to Call"?',
-    'Which artist wrote the song "Country Girl(Shake It For Me)"?',
-    'Which artist wrote the song "No Scrubs"?',
-    'Which artist wrote the song "Cry Me A River"?',
-    'Which artist wrote the song "Thank U, Next"?',
-    'Which artist wrote the song "Stairway to Heaven"?',
-]
+const getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > max_questions) {
+        console.log('finish')
+    }
+    questionCounter++
+    questionNum.innerText = `Question ${questionCounter} of ${max_questions}`
+    const questionIndex = Math.floor(Math.random()* availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    console.log(currentQuestion)
+    question.innerText = currentQuestion.question
+    choices.forEach(choice =>{
+        let choiceID = parseInt(choice.id)
+        if(choiceID === 1){
+            choice.innerText = currentQuestion.option1
+        }else if(choiceID === 2){
+            choice.innerText = currentQuestion.option2
+        }else if(choiceID === 3){
+            choice.innerText = currentQuestion.option3
+        }else if(choiceID === 4){
+            choice.innerText = currentQuestion.option4
+        }
+    })
 
+    choices.forEach(choice =>{
+        choice.addEventListener('click', e => {
+            if(!acceptingAnswers) return
+            acceptingAnswers = false
+            const selectedChoice = event.target
+            let selectedChoiceID = parseInt(selectedChoice.id)
+            const correctAnswer = currentQuestion.correctAnswer
+            const correctAnswerDIV = document.getElementById(`${correctAnswer}`)
+            if(selectedChoiceID === correctAnswer) {
+                selectedChoice.parentElement.classList.add('correct')
+                score += score_points
+                scoreText.innerText = score
+            } else {
+                selectedChoice.parentElement.classList.add('incorrect')
+                correctAnswerDIV.parentElement.classList.add('correct')
+            }
+    
+            setTimeout(()=>{
+                selectedChoice.parentElement.classList.remove('incorrect')
+                correctAnswerDIV.parentElement.classList.remove('correct')
+                getNewQuestion()
+            },5000)
+        })
+    })
+
+
+
+    availableQuestions.splice(questionIndex, 1)
+    acceptingAnswers = true
+}
+
+startGame()
---
+++ plan.md
@@ -0,0 +1,41 @@
+<!-- HTML/CSS Section -->
+
+1. Start with creating the css framework for the game
+2. Create a section for the title
+3. Create a section for the current round
+4. Create a section for the current score and previous round scores
+5. Create a section with the option to restart the game
+
+<!-- Game Outline -->
+<!-- V2 Preference: Start on a different page that explains the rules and has a start button. When 'Start' is clicked, navigate to the game page -->
+
+1. There are 10 questions per round
+2. 20 seconds to answer each question (may need to shorten if too easy)
+3. First round includes multiple choice questions to guess which artist sang the song (1 points per question)
+<!-- V2 Preferences: Try to build out 2 additonal rounds if time permits -->
+4. Second round includes 'typing' game to finish the lyric (3 points per question)
+5. Third round includes general music trivia with varying types of questions - true/false, multiple choice, type the answer, etc (5 points per question)
+    <!-- V2 Preference: scan through questions with a slowing down interval to get a random question  -->
+6. If the correct answer is selected, change the choice div that was clicked to green
+7. If the incorrect answer is selected, change the choice div that was clicked to red
+8. Once a question has been asked remove it from the remaining available questions (maybe create a new array or remove it from the existing array of questions)
+9. Go through each question, updating the score by 1, 3 or 5 if correct answer is selected
+10. Complete the round and show the player's score before moving to the next set of questions
+11. After all 3 rounds, show player's final score
+ <!-- V2 Preference: Have a list of top scores - add each players's final score to the list (maybe make an array and sort after a game is over to have highest 5 scores show) -->
+
+
+<!-- Game Play -->
+1. Start the score at 0
+2. Start the round at 1
+3. Start the available questions at 10
+4. Show what question the player is on (ie. Question 3 of 10)
+5. Create an interval for each round of points (ie. max points for round 1 = 1)
+6. If the correct answer is selected, change the choice div that was clicked to green and add to score based on round's max point interval
+7. Lower available questions by 1
+8. If the incorrect answer is selected, change the choice div that was clicked to red, show the correct choice in green and do not change score
+9. Lower available questions by 1
+10. If an answer has been selected by the player, prevent the player from making another choice
+11. If an answer has been selected before time runs out, stop the clock
+12. SetTimeout before moving on to the next question (reset the clock)
+13. Show player their score at the end of the round
